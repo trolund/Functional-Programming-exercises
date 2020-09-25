7910b7b8c6fbb9c6c0d5ce328950dbd8ca65093f
Troels Lund <trolund@gmail.com>
2020-09-25 15:05:31 +0200

    maybe

--- Polynomials/poly.fsx
+++ Polynomials/poly.fsx
@@ -36,57 +36,39 @@ let rec mulX a =
 
 mulX [ 2; 0; 0; 1 ]
 
-let rec mul a b =
-    match a, b with
+// let rec mul a b =
+//     match a, b with
+//     | [], [] -> []
+//     | [], b -> b
+//     | a, [] -> a
+//     | a :: atail, b :: btail -> (a * b + mul atail btail) :: (mul atail btail)
+
+// mul [ 2; 0; 0; 1 ] [ 2; 0; 0; 1 ]
+
+let rec mul x y =
+    match x, y with
     | [], [] -> []
-    | [], b -> b
     | a, [] -> a
-    | a :: atail, b :: btail -> (a * b + mul atail btail) :: (mul atail btail)
+    | [], b -> b
+    | a :: atail, q -> add (mulC a q) (mulX (mul atail q))
 
 mul [ 2; 0; 0; 1 ] [ 2; 0; 0; 1 ]
-
-// let rec evalGen x p i =
-//     match p with
-//     | [] -> 0
-//     | exp :: tail -> (exp * pow x i + evalGen x tail (i + 1))
-
-// let eval x poly = evalGen x poly 1
-
+mul [ 2; 3; 1 ] [ 1; 2; 3 ]
 
 let eval x poly =
 
     let rec pow x exp = int (float (x) ** float (exp))
 
-    let rec evalGen x p i =
+    let rec cal x p i =
         match p with
         | [] -> 0
-        | exp :: tail -> (exp * pow x i + evalGen x tail (i + 1))
+        | exp :: tail -> (exp * pow x i + cal x tail (i + 1))
 
-    evalGen x poly 1
+    cal x poly 1
 
 eval 2 [ 2; 3; 0; 1 ]
-
-let addThreeNumbers x y z =
-
-    //create a nested helper function
-    let add n = fun x -> x + n
-
-    // use the helper function
-    x |> add y |> add z
-
-pow 5 2
-
-let rec fact x = if x < 1 then 1 else x * fact (x - 1)
-
-
-// eval 2 [ 2; 3; 0; 1 ] // 16
-// eval 2 [ 0; 1; 0; 1 ] // 11
-
-
-
 // Part 2: functional decomposition
-
-let rec toString p: int list =
-    match p with
-    | [] -> []
-    | p :: tail -> (sprintf "%i" + p) + toString tail
+// let rec toString p: int list =
+//     match p with
+//     | [] -> []
+//     | p :: tail -> (sprintf "%i" + p) + toString tail
