d87ac8779553d6d26fe66551b991e22184bb034d
Troels Lund <trolund@gmail.com>
2020-10-29 16:08:59 +0100

    update

--- Week exercies/Week 5/summer2015.fsx
+++ Week exercies/Week 5/summer2015.fsx
@@ -11,6 +11,13 @@ type Mandatory = Set<CourseNo>
 type Optional = Set<CourseNo>
 type CourseGroup = Mandatory * Optional
 
+type BasicNaturalScience = CourseGroup
+type TechnologicalCore = CourseGroup
+type ProjectProfessionalSkill = CourseGroup
+type Elective = CourseNo -> bool
+type FlagModel = BasicNaturalScience * TechnologicalCore * ProjectProfessionalSkill * Elective
+type CoursePlan = Set<CourseNo>
+
 (* 
     1. Declare a function isValidCourseDesc: CourseDesc -> bool,
     where isValidCourseDesc desc is true if the ECTS part of desc is valid 
@@ -58,3 +65,62 @@ let disjoint s1 s2 =
 disjoint op ma
 disjoint op op
 disjoint ma ma
+
+(* 
+    4. Declare a function sumECTS: Set<CourseNo> -> CourseBase -> int,
+    where sumECTS cs cb is the sum of all ECTS points of the courses with numbers in cs,
+    where the ECTS points are extracted from course descriptions in the course base cb.
+*)
+
+let courseNos = Set.ofList [02157; 02131]
+let courseNos1 = Set.ofList [02157; 02158]
+let courseBase: CourseBase = Map.ofList [02157, ("Functional Programming", 5); 02131, ("Embedded Systems", 10); 02141, ("Computer Science Modelling", 10); 02158, ("Parallel programming", 5)]
+
+let sumECTS (cs: Set<CourseNo>, cb: CourseBase) = 
+    Map.fold (fun acc no desc -> 
+                let (_, ects) = desc
+                if cs.Contains no then ects + acc else acc
+                ) 0 cb
+
+sumECTS (courseNos, courseBase)
+sumECTS (courseNos1, courseBase)
+
+(*
+    5. A course group (man, opt) for a bachelor programme is valid for a given course base cb
+    if:
+    • man and opt are disjoint,
+    • the sum of all mandatory ECTS points (i.e. the ECTS sum for all courses in man)
+    is less than or equal to 45,
+
+    manTotalrule:
+    • the set of optional courses opt is empty when the mandatory ECTS points add up
+    to 45, and
+    • the total number of ECTS points of mandatory and optional courses should be at
+    least 45.
+
+    Declare a function isValidCourseGroup: CourseGroup -> CourseBase -> bool that
+    can check whether a course group is valid for a given course base.
+*)
+
+let isValidCourseGroup (cg: CourseGroup, cb: CourseBase) =
+    let (man, opt) = cg
+    let res = disjoint man opt
+    let optSum = sumECTS (opt, cb)
+    let manSum = sumECTS (man, cb)
+    let mandatoryECTSpoints = manSum <= 45 
+    let totalSum = manSum + optSum
+    let manTotalrule = opt.IsEmpty && manSum >= 45 && totalSum >= 45
+    
+    res && mandatoryECTSpoints && manTotalrule
+
+(*
+    6. Declare a function isValid: FlagModel -> CourseBase -> bool that can test whether
+    a flag model is valid for a given course base.
+
+    A course plan cs satisfies a (valid) flag model of a bachelor programme (for a given course
+    base), if the number of ECTS points earned from the courses in cs is 180, subject to the
+    requirement that 45 points are earned in each course group of the flag model, including
+    the elective courses.
+*)
+
+// let isValid fm cb = 
