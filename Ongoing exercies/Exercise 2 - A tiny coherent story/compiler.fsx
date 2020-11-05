b3efce0a89d940397baa28563e09458602c85067
Troels Lund <trolund@gmail.com>
2020-11-05 17:45:42 +0100

    update

--- Ongoing exercies/Exercise 2 - A tiny coherent story/compiler.fsx
+++ Ongoing exercies/Exercise 2 - A tiny coherent story/compiler.fsx
@@ -1,50 +1,106 @@
-module Compiler = 
+module Compiler =
 
-    type Instruction = | ADD 
-                       | SUB 
-                       | SIGN 
-                       | ABS 
-                       | PUSH of int
+    type Instruction =
+        | ADD
+        | SUB
+        | SIGN
+        | ABS
+        | PUSH of int
 
     type Stack = S of int list
 
-    let theStack = [1;2;3;4;5]
+    let theStack = S([ 1; 2; 3; 4; 5 ])
 
-    let sigleEle s a = 
-        match s with 
-        | [] -> S([])
-        | x::tail -> let sec = tail.Head
-                     let restTail = tail.Tail
-                     let firstelm = a sec x
-                     S(firstelm :: restTail)
+    let instru =
+        [ PUSH 2
+          PUSH 4
+          PUSH 8
+          SUB
+          ADD
+          SUB
+          ABS
+          SIGN
+          ABS ]
+
+    let instru2 =
+        [ PUSH 2
+          PUSH 4
+          PUSH 10
+          ADD
+          ADD
+          ABS ]
+
+    let ofList l = S l
+    
+    let toList = function
+        | S(l) -> l
+
+
+    let sigleEle s a =
+        match s with
+        | S([]) -> S([])
+        | S(x :: tail) ->
+            let sec = tail.Head
+            let restTail = tail.Tail
+            let firstelm = a sec x
+            S(firstelm :: restTail)
 
     let add s = sigleEle s (+)
     let sub s = sigleEle s (-)
-    
+
     add theStack
     sub theStack
 
     let first s a =
-        match s with 
-        | [] -> S([])
-        | x::tail -> S((a x)::tail)
-     
-    let sign s = first s (( ~- ))
+        match s with
+        | S([]) -> S([])
+        | S(x :: tail) -> S((a x) :: tail)
+
+    let sign s = first s ((~-))
 
     let abs s = first s (abs)
 
     sign theStack
-    abs [-1;3;4;5]
+    abs (S([ -1; 3; 4; 5 ]))
 
-    let push (r:int) s = S(List.rev (List.append (List.rev s) [r]))
+    let push (r: int) (s: Stack) =
+        let l = toList(s)
+        S(List.rev (List.append (List.rev l) [ r ]))
 
     push 8 theStack
+    push 5 (S([]))
 
-    let intpInstr s (i:Instruction) =
-        match i with 
+    let intpInstr (s: Stack) i =
+        match i with
         | ADD -> add s
         | SUB -> sub s
         | SIGN -> sign s
         | ABS -> abs s
-        | PUSH(i) -> push i s
-    
+        | PUSH (x) -> push x s
+
+    intpInstr (S([])) (PUSH 8)
+
+    (*
+        A program for the stack machine is a list of instructions [i1, i2, . . . , in]. A program is executed
+        by executing the instructions i1, i2, . . . , in one after the other, in that order, starting with an
+        empty stack. The result of the execution is the top value of the stack when all instructions
+        have been executed.
+
+        Declare an F# function to interpret the execution of a program:
+
+            exec: Instruction list -> int
+    *)
+
+    let rec exec i =
+        let sStack = S([])
+
+        let rec aux (i: Instruction list) (s: Stack) = 
+            match (i, s) with 
+            | [], S([]) -> failwith "Sometinhg went wrong!"
+            | [],S(s) -> s.Head
+            | ins::tail, s -> aux tail (intpInstr s ins)
+
+        aux i sStack
+
+    exec instru
+    exec instru2
