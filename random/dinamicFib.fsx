403685dbc3c38e1d3cd33c8e4cffacea530dba10
Troels Lund <trolund@gmail.com>
2020-11-06 16:20:02 +0100

    update

--- random/dinamicFib.fsx
+++ random/dinamicFib.fsx
@@ -1,5 +1,5 @@
-let rec fib (n: int, memo: int []) =
-    if n <= 0 then 0
-    elif n = 1 then 1
-    elif memo [ n ] then memo [ n ] = fib (n - 1) + fib (n - 2)
-        return memo [ n ]
+// let rec fib (n: int, memo: int []) =
+//     if n <= 0 then 0
+//     elif n = 1 then 1
+//     elif memo [ n ] then memo [ n ] = fib (n - 1) + fib (n - 2)
+//          return memo [ n ]
--- random/map.fsx
+++ random/map.fsx
@@ -1,5 +1,7 @@
 // mappings
 
+let testList = [ 1; 2; 3; 4; 5 ]
+
 List.map (fun x -> x = 1) testList
 
 List.map (fun x -> x + 1) testList
