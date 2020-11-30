d4e4c637e4c93c39a0fe0b1d93e834b16ef03e32
Troels Lund <trolund@gmail.com>
2020-11-30 22:07:22 +0100

    update

--- Exams/My solutions/Summer2015/Code/P1.fsx
+++ Exams/My solutions/Summer2015/Code/P1.fsx
@@ -8,3 +8,15 @@ let repeat s t =
     loop "" t
 
 repeat "ab" 4
+
+
+let f s1 s2 t =
+
+    let rec loop n acc =
+        match n with
+        | 0 -> acc
+        | _ -> if n % 2 = 0 then loop (n - 1) (s1 + "\n" + acc) else loop (n - 1) (s2 + "\n" + acc)
+
+    loop t ""
+
+f "XO" "OX" 3
