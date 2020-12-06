f8c93f66e66d9ed87d41494cc2db16e1e739a155
Troels Lund <trolund@gmail.com>
2020-12-06 17:53:00 +0100

    updated

--- Exams/My solutions/Summer2014/code/P3.fsx
+++ Exams/My solutions/Summer2014/code/P3.fsx
@@ -20,7 +20,10 @@ let myTree =
               F,
               1830,
               [ P("Troels", M, 1994, [])
-                P("Emil", M, 1995, []) ]) ]) // Well-formend
+                P("Emil", M, 1995, []) ])
+           P("Hennig", M, 1840, [])
+           P("Flemming", M, 1850, [])
+           P("Gustav", M, 1960, []) ]) // Well-formend
 
 let myTree2 =
     P
@@ -66,3 +69,47 @@ isWF myTree2
 isWF myTree3
 
 let makePerson (n, s, yofb) = P(n, s, yofb, [])
+
+
+let rec insertChildOf n c t =
+    match t with
+    | P (name, s, yofb, cs) when name = n -> Some(P(n, s, yofb, insertInOrder c cs))
+    | P (n, s, yofb, cs) ->
+        match insertChildOfInList n c cs with
+        | None -> None
+        | Some cs' -> Some(P(n, s, yofb, cs'))
+
+and insertChildOfInList n c cs =
+    match cs with
+    | [] -> None
+    | c :: cs ->
+        match insertChildOf n c c with
+        | None ->
+            match insertChildOfInList n c cs with
+            | None -> None
+            | Some cs' -> Some(c :: cs')
+        | Some c' -> Some(c' :: cs)
+
+and insertInOrder c cs =
+    match cs with
+    | [] -> []
+    | curP :: tail ->
+        let (P (_, _, pyofb, cs)) = c
+        let (P (_, _, cyofb, cs)) = curP
+        if cyofb < pyofb then curP :: (insertInOrder c tail) else c :: curP :: tail
+
+let newtree =
+    insertChildOf "Frede" (P("Finn", M, 1956, [])) myTree
+
+let unpackOption =
+    function
+    | Some (v) -> v
+    | None -> failwith "Ingen værdi fundet....."
+
+isWF (unpackOption newtree)
+
+
+let newtree2 =
+    insertChildOf "Frede" (P("Martin", M, 1820, [])) myTree
+
+isWF (unpackOption newtree2)
