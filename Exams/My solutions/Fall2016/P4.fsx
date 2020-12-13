0e6b6b2a9425d3dccfa8324138ca7c5d790e97b9
Troels Lund <trolund@gmail.com>
2020-12-13 21:33:26 +0100

    update

--- Exams/My solutions/Fall2016/P4.fsx
+++ Exams/My solutions/Fall2016/P4.fsx
@@ -1,4 +1,80 @@
+#I "/Users/troelslund/.nuget/packages/fscheck/3.0.0-alpha4/lib/net452"
+#r "FsCheck.dll"
+open FsCheck
+
 type Tree<'a, 'b> =
     | A of 'a
     | B of 'b
     | Node of Tree<'a, 'b> * Tree<'a, 'b>
+
+// 1.
+
+let exp = Node(A "a", B "b")
+let exp2 = A "a"
+let exp3 = Node(A "a", Node(A "a", B "b"))
+
+// 2.
+
+let rec countA =
+    function
+    | Node (t1, t2) -> countA t1 + countA t2
+    | B (_) -> 0
+    | A (_) -> 1
+
+countA exp3
+
+// 3.
+
+let rec subst a a' b b' =
+    function
+    | Node (t1, t2) -> Node(subst a a' b b' t1, subst a a' b b' t2)
+    | B (v) -> if v = b then B b' else B v
+    | A (v) -> if v = a then A a' else A v
+
+
+subst "a" "a'" "b" "b'" exp3
+
+
+// 4.
+
+let rec g =
+    function
+    | Node (t1, t2) -> Node(g t2, g t1)
+    | leaf -> leaf
+
+let rec f =
+    function
+    | A a -> ([ a ], [])
+    | B b -> ([], [ b ])
+    | Node (t1, t2) ->
+        let (xs1, ys1) = f t1
+        let (xs2, ys2) = f t2
+        (xs1 @ xs2, ys1 @ ys2)
+(*
+    g computes:
+
+    g spejler et træ.
+
+
+    f computes:
+
+    f splitter a og b værdier op i hver sin liste.
+
+*)
+
+
+// 5.
+
+let fK t =
+
+    let rec aux t k =
+        match t with
+        | A a -> k ([ a ], [])
+        | B b -> k ([], [ b ])
+        | Node (t1, t2) -> aux t1 (fun (xs1, ys1) -> aux t2 (fun (xs2, ys2) -> k (xs1 @ xs2, ys1 @ ys2)))
+
+    aux t id
+
+let test exp = f exp = fK exp
+
+Check.Quick test
---
+++ Exams/My solutions/Fall2016/P5.fsx
@@ -0,0 +1,92 @@
+type T<'a> = N of 'a * T<'a> list
+
+
+let td = N("g", [])
+let tc = N("c", [ N("d", []); N("e", [ td ]) ])
+let tb = N("b", [ N("c", []) ])
+let ta = N("a", [ tb; tc; N("f", []) ])
+
+
+// 1.
+
+let rec tolist =
+    function
+    | N (n, sub) -> n :: aux sub
+
+and aux =
+    function
+    | [] -> []
+    | x :: xs -> tolist x @ aux xs
+
+tolist ta
+
+// 2.
+
+let map f t =
+    let rec mapList =
+        function
+        | [] -> []
+        | n :: ns -> (nodeMap n) :: (mapList ns)
+
+    and nodeMap =
+        function
+        | N (name, children) -> N(f name, mapList children)
+
+    nodeMap t
+
+
+map (fun x -> x) ta
+map (fun x -> x + " hej") ta
+map (fun x -> if x = "c" then "seeee" else x) ta
+
+
+// alternativ løsning:
+
+(*
+    Løsning hvor funktionen f bliver parset rundt som parameter.
+*)
+
+let rec mapL f =
+    function
+    | [] -> []
+    | n :: ns -> (map2 f n) :: (mapL f ns)
+
+and map2 f =
+    function
+    | N (name, children) -> N(f name, mapL f children)
+
+
+// 3.
+
+let rec isPath is t =
+    match is, t with
+    | [], _ -> true
+    | p :: px, N (name, children) ->
+        match List.tryItem p children with
+        | None -> false
+        | Some (v) -> isPath px v
+
+isPath [ 1; 1; 0 ] ta
+
+// 4.
+
+let rec get is t =
+    match is, t with
+    | [], _ -> t
+    | p :: px, N (name, children) -> get px (List.item p children)
+
+get [ 1; 1; 0 ] ta
+
+// 5.
+
+let rec tryFindPathTo v (N (v', ts)) =
+    if v = v' then Some [] else tryFindInList 0 v ts
+
+and tryFindInList i v =
+    function
+    | [] -> None
+    | N (v', _) :: ts when v = v' -> Some [ i ]
+    | N (_, ts') :: ts ->
+        match tryFindInList 0 v ts' with
+        | None -> tryFindInList (i + 1) v ts
+        | Some is -> Some(i :: is)
